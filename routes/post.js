104f3dc7776fc7025a5a3ca0d48e6fa07e94fb0d
tylertran349 <tylertran349@gmail.com>
2023-04-01 12:24:09 -0700

    Fix delete and put routes

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const { Router } = require('express');
 const router = Router();
-const User = require("../models/user");
 const Post = require("../models/post");
-const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
 require('dotenv').config();
@@ -26,39 +24,75 @@ router.post('/', verifyToken, [
     body('title').isLength({min: 1}).escape().withMessage("Post title must be specified."),
     body('content').isLength({min: 1}).escape().withMessage("Content of post must be specified."),
     body('published').isBoolean().withMessage("published must be a boolean value."),
-], async(req, res) => {
-    try {
-        // Decode the JSON web token
-        jwt.verify(req.token, process.env.JWT_SECRET_KEY, (err, token) => {
-            if(err) {
-                return res.status(403).json({error: "Error 403: Forbidden"}); // Only if token is invalid
-            } else {
-                const validation_errors = validationResult(req);
-                if(!validation_errors.isEmpty()) {
-                    return res.status(400).json({errors: validation_errors.array()});
-                }
-                const {title, content, published} = req.body; // Destructure properties from req.body object
-
-                // Create new post
-                const post = new Post({
-                    title,
-                    content,
-                    date: new Date(),
-                    likes: 0,
-                    published,
-                    user: token.user, // token.user = user associated with decoded token
-                    comments: [],
-                });
-                post.save().then(function() {
-                    res.json(post);
-                }, function(err) {
-                    return res.status(500).json({error: "Could not create new post in database"}); // New post could not be created/added to the database
-                });
+], (req, res) => {
+    jwt.verify(req.token, process.env.JWT_SECRET_KEY, (err, token) => {
+        if(err) {
+            return res.status(403).json({error: "Error 403: Forbidden"}); // Only if token is invalid
+        }
+        try {
+            const validation_errors = validationResult(req);
+            if(!validation_errors.isEmpty()) {
+                return res.status(400).json({errors: validation_errors.array()});
             }
-        });
-    } catch {
-        res.status(500).json({error: "Server error"});
-    }
+            const {title, content, published} = req.body; // Destructure properties from req.body object
+
+            // Create new post
+            const post = new Post({
+                title,
+                content,
+                date: new Date(),
+                likes: 0,
+                published,
+                user: token.user, // token.user = user associated with decoded token
+                comments: [],
+            });
+            post.save().then(function() {
+                res.json(post);
+            }, function(err) {
+                return res.status(500).json({error: "Could not create new post in database"}); // New post could not be created/added to the database
+            });
+        } catch {
+            res.status(500).json({error: "Server error"});
+        }
+    });
+});
+
+router.put('/:postId', verifyToken, [
+    body('title').isLength({min: 1}).escape().withMessage("Post title must be specified."),
+    body('content').isLength({min: 1}).escape().withMessage("Content of post must be specified."),
+    body('published').isBoolean().withMessage("published must be a boolean value."),
+], async(req, res) => {
+    jwt.verify(req.token, process.env.JWT_SECRET_KEY, async(err, token) => {
+        if(err) {
+            return res.status(403).json({error: "Error 403: Forbidden"});
+        } 
+        try {
+            const post = await Post.findByIdAndUpdate(req.params.postId, req.body, {new: true});
+            if(!post) {
+                return res.status(404).send({error: "Post not found"});
+            }
+            res.json(post);
+        } catch {
+            res.status(500).json({error: "Server error"});
+        }
+    })
+});
+
+router.delete('/:postId', verifyToken, (req, res) => {
+    jwt.verify(req.token, process.env.JWT_SECRET_KEY, async(err, token) => {
+        if(err) {
+            return res.status(403).json({error: "Error 403: Forbidden"});
+        } 
+        try {
+            const post = await Post.findByIdAndRemove(req.params.postId);
+            if(!post) {
+                return res.status(404).send({error: "Post not found"});
+            }
+            res.json(post);
+        } catch {
+            res.status(500).json({error: "Server error"});
+        }
+    });
 });
 
 module.exports = router;
