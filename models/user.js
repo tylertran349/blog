b5e8c1f61ce5ee30b3121d4b18f0d8e9cd59a79a
tylertran349 <tylertran349@gmail.com>
2023-04-01 12:55:07 -0700

    Remove liked_comments and liked_posts fields

--- models/user.js
+++ models/user.js
@@ -6,8 +6,6 @@ const UserSchema = new Schema({
     first_name: {type: String, required: true},
     last_name: {type: String, required: true},
     password: {type: String, required: true},
-    liked_posts: [{type: Schema.Types.ObjectId, ref: "Post", required: true}],
-    liked_comments: [{type: Schema.Types.ObjectId, ref: "Comment", required: true}],
     is_admin: {type: Boolean, required: true},
 });
 
