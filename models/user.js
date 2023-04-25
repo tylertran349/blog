a26124e64f64f5afa64f8444b376ba55c0a91187
tylertran349 <tylertran349@gmail.com>
2023-04-24 22:01:41 -0700

    Add posts and comments fields

--- models/user.js
+++ models/user.js
@@ -7,6 +7,8 @@ const UserSchema = new Schema({
     last_name: {type: String, required: true},
     password: {type: String, required: true},
     is_admin: {type: Boolean, required: true},
+    posts: [{type: Schema.Types.ObjectId, ref: "Post", required: true}],
+    comments: [{type: Schema.Types.ObjectId, ref: "Comment", required: true}],
 });
 
 module.exports = mongoose.model("User", UserSchema);
