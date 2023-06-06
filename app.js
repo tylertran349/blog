d67e20aeed49aea2a66264fd5ece57d5bc3a5866
tylertran349 <tylertran349@gmail.com>
2023-06-05 21:27:04 -0700

    Move CORS middleware as high up as possible to fix CORS bug

--- app.js
+++ app.js
@@ -9,12 +9,6 @@ const cors = require('cors');
 
 app.use(cors());
 
-main().catch(err => console.log(err));
-
-async function main() {
-    await mongoose.connect(mongoDB);
-}
-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Add the "Access-Control-Allow-Origin" header to all responses, allowing any domain to access the API's resources
     res.header(
@@ -25,8 +19,15 @@ app.use((req, res, next) => {
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization, token"
     );
+    res.header("Access-Control-Allow-credentials", true);
     next();
 });
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(mongoDB);
+}
 
 app.use(express.static('public'));
 
